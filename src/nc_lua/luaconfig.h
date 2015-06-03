40213bc5f125fbe2d9c18a935e24c0984c1d7ea8
Nicholas Gill <mythagel@gmail.com>
2015-06-03 22:55:46 +1000

    Start lua wrapper

---
+++ src/nc_lua/luaconfig.h
@@ -0,0 +1,47 @@
+/* 
+ * Copyright (C) 2013  Nicholas Gill
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * luaconfig.h
+ *
+ *  Created on: 2015-06-02
+ *      Author: nicholas
+ */
+
+#ifndef LUACONFIG_H_
+#define LUACONFIG_H_
+#include <string>
+#include "luastate.h"
+
+namespace lua {
+
+/* Add functions to read value from lua table
+ * at creation creates a lua state and runs config file
+ * allows querying of configuration tables as
+ * OPTIONAL table name, followed by value as type.
+ * */
+class config {
+private:
+    state L;
+public:
+    config();
+    ~config();
+};
+
+}
+
+#endif /* LUACONFIG_H_ */
---
+++ src/nc_lua/luastate.cpp
@@ -0,0 +1,50 @@
+/* 
+ * Copyright (C) 2013  Nicholas Gill
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * luastate.cpp
+ *
+ *  Created on: 2015-06-03
+ *      Author: nicholas
+ */
+
+#include "luastate.h"
+#include <lua.hpp>
+#include <utility>
+
+namespace lua {
+
+state::state()
+ : L(luaL_newstate()){
+}
+state::state(state&& s) {
+    using std::swap;
+    swap(L, s.L);
+}
+state& state::operator=(state&& s) {
+    using std::swap;
+    swap(L, s.L);
+    return *this;
+}
+state::operator lua_State*() {
+    return L;
+}
+state::~state() {
+    lua_close(L);
+}
+
+}
