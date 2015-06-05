3cbca5f0e59fac34781bdfddf4bbe267930aa122
Nicholas Gill <mythagel@gmail.com>
2015-06-05 12:27:00 +1000

    Update tools.lua

--- tools.lua
+++ tools.lua
@@ -1,6 +1,19 @@
+-- Complete general configuration for nc tools
+-- searched for in cwd of tool, or specified with config file 
+-- (cwd more convenient - expected that tools will be executed from project root)
+-- not all configuration used by all tools BUT ONE config file for all tools with different
+-- sections set.
 
+-- Machine configuration
+machine = {
+	type = "mill",
+	spindle = {"100-1000", "2000-6000"}
+}
+
+-- Tool table - tool definitions could be imported from other files...
 tool_table = {
     [3] = {
+        name = "8mm carbide end mill",
         length = 50,
         diameter = 8,
         flute_length = 20,
