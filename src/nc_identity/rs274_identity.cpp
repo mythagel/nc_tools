242fab43060eac94f5eb2f01e16defbc59013f51
Nicholas Gill <mythagel@gmail.com>
2015-11-12 20:45:23 +1100

    Add nc_identity tool

---
+++ src/nc_identity/rs274_identity.cpp
@@ -0,0 +1,45 @@
+/* 
+ * Copyright (C) 2013  Nicholas Gill
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * rs274_identity.cpp
+ *
+ *  Created on: 2015-11-12
+ *      Author: nicholas
+ */
+
+#include "rs274_identity.h"
+#include <cmath>
+#include <cstring>
+#include "throw_if.h"
+#include "fold_adjacent.h"
+#include <algorithm>
+
+void rs274_identity::_rapid(const Position&) {
+}
+
+void rs274_identity::_arc(const Position&, const Position&, const cxxcam::math::vector_3&, int) {
+}
+
+
+void rs274_identity::_linear(const Position&) {
+}
+
+rs274_identity::rs274_identity()
+ : rs274_base() {
+}
+
