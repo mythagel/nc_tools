e7c3025a1ee93dc95e24a7f72d77ba13065584b6
Nicholas Gill <mythagel@gmail.com>
2016-04-06 20:44:16 +1000

    Improve error handling

--- src/nc_lathe_roughing/rs274_path.cpp
+++ src/nc_lathe_roughing/rs274_path.cpp
@@ -32,21 +32,41 @@ void rs274_path::_rapid(const Position&) {
     if (!path_.empty())
         throw std::runtime_error("Rapid within profile disallowed");
 }
-void rs274_path::_arc(const Position& end, const Position& center, const cxxcam::math::vector_3&, int) {
-    // TODO handle plane
+void rs274_path::_arc(const Position& end, const Position& center, const cxxcam::math::vector_3& plane, int rotation) {
+    using cxxcam::units::length_mm;
+
+    if (plane.y != 1)
+        throw std::runtime_error("Arc must exist in XZ plane");
+    if (std::abs(end.y - program_pos.y) > 0)
+        throw std::runtime_error("Helix not supported in path");
+    if (std::abs(rotation) > 1)
+        throw std::runtime_error("Single rotation only in path");
+
+    auto a = convert(program_pos);
+    auto b = convert(end);
+    auto c = convert(center);
     arc_2 arc;
-    arc.a = {program_pos.x, program_pos.z};
-    arc.b = {end.x, end.z};
-    arc.c = {center.x, center.z};
+    arc.dir = rotation < 0 ? arc_2::cw : arc_2::ccw;
+    arc.a = {length_mm(a.X).value(), length_mm(a.Z).value()};
+    arc.b = {length_mm(b.X).value(), length_mm(b.Z).value()};
+    arc.c = {length_mm(c.X).value(), length_mm(c.Z).value()};
     path_.push_back(arc);
 }
 
 
 void rs274_path::_linear(const Position& pos) {
-    // TODO verify 2d / yz plane
+    using cxxcam::units::length_mm;
+
+    if (_active_plane != Plane::XZ)
+        throw std::runtime_error("Path must be described in XZ plane");
+    if (std::abs(pos.y - program_pos.y) > 0)
+        throw std::runtime_error("Path must be 2d");
+
+    auto a = convert(program_pos);
+    auto b = convert(pos);
     line_segment_2 line;
-    line.a = {program_pos.x, program_pos.z};
-    line.b = {pos.x, pos.z};
+    line.a = {length_mm(a.X).value(), length_mm(a.Z).value()};
+    line.b = {length_mm(b.X).value(), length_mm(b.Z).value()};
     path_.push_back(line);
 }
 
