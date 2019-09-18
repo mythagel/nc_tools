e99788c64756c3b5d4a7fc6ec807a40c7dc5352d
Nicholas Gill <mythagel@gmail.com>
2019-09-18 18:43:45 +1000

    nc_slice: Calculate stock via offset
    Generate a stock model by projecting and offseting the model to be
    sliced.

--- src/nc_slice/slice.cpp
+++ src/nc_slice/slice.cpp
@@ -29,6 +29,7 @@ int main(int argc, char* argv[]) {
         ("feedrate,f", po::value<double>()->required(), "Feed rate")
         ("stepdown,d", po::value<double>()->required(), "Z Stepdown")
         ("stock", po::value<std::string>(), "Stock model file")
+        ("offset,o", po::value<double>(), "Generate stock via offset")
     ;
 
     try {
@@ -44,14 +45,45 @@ int main(int argc, char* argv[]) {
         double stepdown = vm["stepdown"].as<double>();
         double f = vm["feedrate"].as<double>();
 
+        double scale = 10e12;
+        auto scale_point = [scale] (const geom::polygon_t::point& point){
+            return cl::IntPoint(point.x * scale, point.y * scale);
+        };
+        auto unscale_point = [scale](const cl::IntPoint& p) -> geom::polygon_t::point {
+            return {static_cast<double>(p.X)/scale, static_cast<double>(p.Y)/scale};
+        };
+        auto scale_paths = [scale_point] (const std::vector<geom::polygon_tree_t>& polygons) {
+            cl::Paths paths;
+            for (auto& polygon : polygons) {
+                for (auto& poly : polygon.polygons) {
+                    paths.emplace_back();
+                    for (auto& p : poly.points)
+                        paths.back().push_back(scale_point(p));
+                }
+            }
+            return paths;
+        };
+
         geom::polyhedron_t model;
         throw_if(!(std::cin >> geom::format::off >> model), "Unable to read model from file");
 
         geom::polyhedron_t stock;
-        bool has_stock = vm.count("stock");
-        if (has_stock) {
+        bool has_stock_file = vm.count("stock");
+        if (has_stock_file) {
             std::ifstream is(vm["stock"].as<std::string>());
             throw_if(!(is >> geom::format::off >> stock), "Unable to read stock from file");
+        }
+
+        cl::Paths stock_offset;
+        bool has_stock_offset = vm.count("offset");
+        if (has_stock_offset)
+        {
+            stock_offset = scale_paths(geom::projection_xy(model));
+
+            cl::ClipperOffset co;
+            co.AddPaths(stock_offset, cl::jtRound, cl::etClosedPolygon);
+            co.ArcTolerance = 0.1 * scale;
+            co.Execute(stock_offset, vm["offset"].as<double>() * scale);
         }
 
         auto bbox = geom::bounding_box(model);
@@ -70,32 +102,19 @@ int main(int argc, char* argv[]) {
             auto slice_bounds = geom::make_box({bbox.min.x, bbox.min.y, z1}, {bbox.max.x, bbox.max.y, z0});
             auto slice = geom::projection_xy(model * slice_bounds);
 
-            // If a stock model has been provided, calculate the slice of material to be removed
-            if (has_stock) {
-                auto stock_slice = geom::projection_xy(stock * slice_bounds);
-
-                double scale = 10e12;
-                auto scale_point = [scale] (const geom::polygon_t::point& point){
-                    return cl::IntPoint(point.x * scale, point.y * scale);
-                };
-                auto unscale_point = [&](const cl::IntPoint& p) -> geom::polygon_t::point {
-                    return {static_cast<double>(p.X)/scale, static_cast<double>(p.Y)/scale};
-                };
-                auto scale_paths = [scale_point] (const std::vector<geom::polygon_tree_t>& polygons) {
-                    cl::Paths paths;
-                    for (auto& polygon : polygons) {
-                        for (auto& poly : polygon.polygons) {
-                            paths.emplace_back();
-                            for (auto& p : poly.points)
-                                paths.back().push_back(scale_point(p));
-                        }
-                    }
-                    return paths;
-                };
+            // If stock (model or offset) has been provided, calculate the slice of material to be removed
+            if (has_stock_file || has_stock_offset) {
 
                 cl::Clipper clpr;
-                clpr.AddPaths(scale_paths(stock_slice), cl::ptSubject, true);
                 clpr.AddPaths(scale_paths(slice), cl::ptClip, true);
+
+                if (has_stock_file) {
+                    auto stock_slice = geom::projection_xy(stock * slice_bounds);
+                    clpr.AddPaths(scale_paths(stock_slice), cl::ptSubject, true);
+                }
+                else if (has_stock_offset) {
+                    clpr.AddPaths(stock_offset, cl::ptSubject, true);
+                }
 
                 cl::Paths solution;
                 clpr.Execute(cl::ctDifference, solution);
